410d916b4fc69479022848bc32dc00bbd46ee36d
johnlin <john830110@gmail.com>
2026-04-06 19:50:06 +0800

    feat: 新增簡報內容

---
+++ layouts/TwoCols57.vue
@@ -0,0 +1,36 @@
+<!--
+  Usage:
+```md
+---
+layout: TwoCols57
+---
+# Left
+This shows on the left
+::right::
+# Right
+This shows on the right
+```
+-->
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  layoutClass: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout two-columns w-full h-full grid" :class="props.layoutClass"
+    style="grid-template-columns: 5fr 7fr;">
+    <div class="col-left" :class="props.class">
+      <slot />
+    </div>
+    <div class="col-right" :class="props.class">
+      <slot name="right" />
+    </div>
+  </div>
+</template>

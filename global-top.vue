9cc4c20d6dbbb5baa527ca7c2c87fcd2378d5293
johnlin <john830110@gmail.com>
2026-04-06 19:53:58 +0800

    feat: 新增頁碼

---
+++ global-top.vue
@@ -0,0 +1,15 @@
+<!-- hide the footer from "cover" layout -->
+<template>
+  <Scroll />
+  <footer class="absolute bottom-5 left-0 right-7 text-right">
+    <SlideCurrentNo v-if="$nav.currentPage !== 1" />
+    <!-- / -->
+    <!-- <SlidesTotal /> -->
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { useSlideContext } from "@slidev/client";
+
+const { $nav } = useSlideContext();
+</script>
